3b037dd5b23f195fd889d6737cec391a0b6e0303
Wallacy Freitas <wallacy@snews.tv>
2020-12-14 23:29:21 -0300

    go

--- y.go
+++ y.go
@@ -1,9 +1,6 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
-
 func main() {
-    fmt.Println(quote.Go())
+	const msg = "Don't communicate by sharing memory, share memory by communicating."
+	println(msg)
 }
