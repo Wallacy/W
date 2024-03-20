f8dfe5ac9d4514b086991075a82e660d4f530a1d
Wallacy <wallacy@snews.tv>
2024-03-19 23:09:50 -0300

    WIP

--- C/main.c
+++ C/main.c
@@ -16,7 +16,7 @@ struct file1_exports {
 
 extern const struct file1_exports exports();
 
-int main(){
+int main(int argc, char** argv){ // int wmain(int argc, wchar_t** argv)
     const struct file1_exports module = exports();
     printf("%s\n", module.self->hello);
     printf("%d %d\n", module.getA(module.self), module.getB(module.self));
