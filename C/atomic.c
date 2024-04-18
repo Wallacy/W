8f07f5fe42eb36c4d1516582a8210bcf3c17cc56
Wallacy <wallacy@snews.tv>
2024-04-18 18:11:23 -0300

    WIP

--- C/atomic.c
+++ C/atomic.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdatomic.h>
 #include <pthread.h>
+// #include <execinfo.h>
+// #include <stdlib.h>
 
 _Atomic int acnt;
 int cnt;
@@ -20,6 +22,14 @@ int main()
         pthread_create(&tid[i],NULL,adding,NULL);
     for(int i=0; i<10; i++)
         pthread_join(tid[i],NULL);
+        
+    // void* callstack[128];
+    // int i, frames = backtrace(callstack, 128);
+    // char** strs = backtrace_symbols(callstack, frames);
+    // for (i = 0; i < frames; ++i) {
+    //     printf("%s\n", strs[i]);
+    // }
+    // free(strs);
 
     printf("the value of acnt is %d\n", acnt);
     printf("the value of cnt is %d\n", cnt);
