aa82108c4ca9066bae20588c10b00c445da349cf
Wallacy <wallacy@snews.tv>
2025-03-11 22:09:58 -0300

    Não diferenciando por tipo especifico e sim se é escalar ou não.

--- _w_/C/tagged_pointer.c
+++ _w_/C/tagged_pointer.c
@@ -22,10 +22,8 @@
 #endif
 
 #define CHECK_CPU 1
-#define TYPE_BITS 3
-#define MAX_TAGS ((1UL << (64 - ADDRESS_BITS - TYPE_BITS - 1)) - 1)
-#define ADDRESS_TAGS ((1UL << ADDRESS_BITS) -1 )
-
+#define true 1
+#define false 0
 typedef struct {
     _Atomic uint32_t ref_count;
     _Atomic uint32_t type;
@@ -88,209 +86,197 @@ static inline int get_virtual_address_bits() {
 }
 #endif
 
+// ***** Definições de TaggedPointer ***** //
 typedef union TaggedPointer {
-    _Atomic uintptr_t raw;
+    _Atomic uintptr_t raw; // Valor bruto atômico
     struct {
-#if defined(__x86_64__) || defined(__aarch64__)
-        uintptr_t address : ADDRESS_BITS;
-        uint16_t tags     : (64 - ADDRESS_BITS - TYPE_BITS - 1);
-        uint8_t type      : TYPE_BITS;
-        uint8_t is_erro   : 1;
-#elif defined(__arm__) || defined(__i386__)
-        uint32_t address  : 30;
-        uint32_t tags     : (32 - 30 - TYPE_BITS - 1);
-        uint32_t type     : TYPE_BITS;
-        uint32_t is_erro  : 1;
+#if defined(__x86_64__) || defined(__aarch64__) // Arquitetura 64 bits
+        uint64_t value : (64 - 2);  // 62 bits para escalares (64 - 2 flags)
+        uint8_t is_compound : 1;    // Flag: 1 bit
+        uint8_t is_error : 1;       // Flag: 1 bit
+#elif defined(__arm__) || defined(__i386__) // Arquitetura 32 bits
+        uint32_t value : (32 - 2);  // 30 bits para escalares (32 - 2 flags)
+        uint8_t is_compound : 1;    // Flag: 1 bit
+        uint8_t is_error : 1;       // Flag: 1 bit
+#endif
+    };
+    struct {
+#if defined(__x86_64__) || defined(__aarch64__) // Arquitetura 64 bits
+        uintptr_t address : ADDRESS_BITS;          // Bits para endereço
+        uint16_t tags : (64 - ADDRESS_BITS - 2);   // Bits restantes para tags
+        uint8_t       : 1;                   // Flag: 1 bit
+        uint8_t       : 1;                      // Flag: 1 bit
+#elif defined(__arm__) || defined(__i386__) // Arquitetura 32 bits
+        uint32_t address : ADDRESS_BITS;           // Bits para endereço
+        uint8_t tags : (32 - ADDRESS_BITS - 2);    // Bits restantes para tags
+        uint8_t      : 1;                   // Flag: 1 bit
+        uint8_t      : 1;                      // Flag: 1 bit
 #endif
     };
 } __attribute__((aligned(sizeof(uintptr_t)))) TaggedPointer;
 
-enum Type {
-    TYPE_NULL = 0, TYPE_INT = 1, TYPE_FLOAT = 2, TYPE_CHAR = 3, TYPE_ARRAY = 4, TYPE_CUSTOM = 7
-};
+static inline void set_error(TaggedPointer* tp, uint32_t error_code, const char* message);
 
-static inline bool is_null(TaggedPointer* tp) { return tp->address == 0; }
-static inline bool is_error(TaggedPointer* tp) { return tp->is_erro; }
-static inline bool is_tagged_value(TaggedPointer* tp) { return tp->address == ((1ULL << ADDRESS_BITS) - 1); }
-static inline bool is_indirect(TaggedPointer* tp) { return tp->address != 0 && tp->address != ((1ULL << ADDRESS_BITS) - 1); }
+static inline bool is_null(TaggedPointer* tp) {
+    if (tp->is_compound) {
+        return tp->address == 0;
+    }
+    return 0;  // Escalares não são nulos
+}
+
+static inline bool is_error(TaggedPointer* tp) {
+    return tp->is_error;
+}
+
+static inline bool is_scalar(TaggedPointer* tp) {
+    return !tp->is_compound && !tp->is_error;
+}
+
+static inline bool is_indirect(TaggedPointer* tp) {
+    return tp->is_compound && tp->address != 0 && !tp->is_error;
+}
 
 static inline void* get_real_pointer(TaggedPointer* tp) {
-    if (!is_indirect(tp)) return NULL;
+    if (!tp->is_compound) return NULL; // Não é um ponteiro indireto
 #if defined(__x86_64__) || defined(__aarch64__)
     uintptr_t addr = tp->address;
-    if (addr & (1ULL << (ADDRESS_BITS - 1))) {
+    if (addr & (1ULL << (ADDRESS_BITS - 1))) { // Extensão de sinal
         addr |= ~((1ULL << ADDRESS_BITS) - 1);
     } else {
         addr &= (1ULL << ADDRESS_BITS) - 1;
     }
     return (void*)addr;
 #elif defined(__arm__) || defined(__i386__)
-    return (void*)(tp->address << 2);
+    return (void*)(tp->address); // Ajuste simples para 32 bits
 #endif
 }
 
 static inline uintptr_t get_value(TaggedPointer* tp) {
-    if (is_null(tp) || is_error(tp)) {
+    if (is_error(tp)) {
         return 0;
     }
-    if (is_tagged_value(tp)) {
-        return tp->tags;
+    if (is_scalar(tp)) {
+        return tp->value;
     }
     if (is_indirect(tp)) {
-        if (tp->type == TYPE_CUSTOM) {
-            SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
-            return *(uintptr_t*)sp->address;
-        } else if (tp->type == TYPE_INT) {
-            return *(int*)get_real_pointer(tp);
-        } else if (tp->type == TYPE_FLOAT) {
-            return *(float*)get_real_pointer(tp);
-        } else {
-            return (uintptr_t)get_real_pointer(tp);
-        }
+        return (uintptr_t)get_real_pointer(tp);
     }
     return 0;
 }
 
 static inline void INFO(TaggedPointer* tp) {
-    printf("TaggedPointer { address: 0x%lx, tags: %u, type: %u, is_erro: %d, value: %lu",
-           tp->address, tp->tags, tp->type, tp->is_erro, get_value(tp));
-    if (tp->type == TYPE_CUSTOM && is_indirect(tp)) {
-        SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
-        printf(", ref_count: %u", sp->ref_count);
+    printf("TaggedPointer { ");
+    if (tp->is_error) {
+        Error* err = (Error*)get_real_pointer(tp);
+        printf("is_error: 1, error_code: %u", err->error_code);
+    } else if (tp->is_compound) {
+        printf("is_compound: 1, address: 0x%lx, tags: %u", tp->address, tp->tags);
+        if (is_indirect(tp) && tp->tags == 1) {
+            SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
+            printf(", ref_count: %u", sp->ref_count);
+        }
+    } else {
+        printf("is_compound: 0, value: %llu", tp->value);
     }
     printf(" }\n");
 }
 
-static inline TaggedPointer create(size_t size, uint16_t tags, uint8_t type) {
+static inline TaggedPointer create(bool is_compound, size_t size, uint16_t tags) {
     TaggedPointer tp = { .raw = 0 };
-    tp.address = ((uintptr_t)1 << ADDRESS_BITS) - 1;  // Valor inicial inválido
-    tp.tags = tags > MAX_TAGS ? MAX_TAGS : tags;
-    tp.type = type;
-    tp.is_erro = 0;
-
-    if (type == TYPE_CUSTOM) {
-        SharedPointer* sp = CALLOC(sizeof(SharedPointer));
-        if (sp) {
-            sp->address = (uintptr_t)CALLOC(size);  // Aloca memória para os dados
-            sp->ref_count = 1;                      // Inicializa o contador de referências
-            sp->type = type;                        // Define o tipo
-            tp.address = (uintptr_t)sp & ((1ULL << ADDRESS_BITS) - 1);  // Armazena apenas ADDRESS_BITS
+    if (is_compound) {
+        tp.is_compound = 1;
+        tp.is_error = 0;
+        if (size > 0) {
+            void* ptr = CALLOC(size);
+            if (ptr) {
+                tp.address = (uintptr_t)ptr & ((1ULL << ADDRESS_BITS) - 1);
+                tp.tags = tags;
+            }
+        } else {
+            tp.address = 0;  // null
         }
-    } else if (size > sizeof(uintptr_t)) {
-        void* ptr = CALLOC(size);
-        if (ptr) {
-            tp.address = (uintptr_t)ptr & ((1ULL << ADDRESS_BITS) - 1);  // Armazena apenas ADDRESS_BITS
-        }
+    } else {
+        tp.is_compound = 0;
+        tp.is_error = 0;
+        tp.value = 0;
     }
     return tp;
 }
 
-static inline void set_error(TaggedPointer* tp, uint32_t error_code, const char* message) {
-    // Se for indireto, liberar a memória anterior
-    if (is_indirect(tp)) {
-        if (is_error(tp)) {
-            Error* err = (Error*)get_real_pointer(tp);
-            FREE(err->message); // Liberar a string duplicada
-            FREE(err);          // Liberar a estrutura Error
+static inline void set_value(TaggedPointer* tp, void* value, size_t size) {
+    if (tp->is_error) return;
+    if (!tp->is_compound) { // Escalar
+#if defined(__x86_64__) || defined(__aarch64__)
+        if (size <= sizeof(uint64_t)) {
+            uint64_t temp;
+            memcpy(&temp, value, size);
+            tp->value = temp;
         } else {
-            FREE(get_real_pointer(tp)); // Liberar qualquer outro ponteiro
+            tp->is_error = 1;
         }
+#elif defined(__arm__) || defined(__i386__)
+        if (size <= sizeof(uint32_t)) {
+            uint32_t temp;
+            memcpy(&temp, value, size);
+            tp->value = temp;
+        } else {
+            tp->is_error = 1;
+        }
+#endif
+    } else { // Ponteiro indireto
+        void* ptr = get_real_pointer(tp);
+        if (ptr) memcpy(ptr, value, size);
     }
-    // Alocar novo erro
+}
+
+static inline void set_error(TaggedPointer* tp, uint32_t error_code, const char* message) {
+    if (is_indirect(tp)) {
+        FREE(get_real_pointer(tp));
+    }
     Error* err = CALLOC(sizeof(Error));
     if (err) {
         err->error_code = error_code;
         err->message = strdup(message);
-        err->prev_address = tp->address; // Preservar o endereço anterior, se necessário
-        tp->address = (uintptr_t)err;
-        tp->is_erro = 1;
-        tp->type = TYPE_NULL;
+        err->prev_address = tp->address;
+        tp->address = (uintptr_t)err & ((1ULL << ADDRESS_BITS) - 1);
+        tp->is_error = 1;
+        tp->is_compound = 1;
     }
-}
-
-static inline void set_value(TaggedPointer* tp, void* value, size_t size, uint8_t type) {
-    if (is_error(tp)) return;
-    if (size <= sizeof(uint16_t) && type != TYPE_CUSTOM) {
-        tp->address = ((uintptr_t)1 << ADDRESS_BITS) - 1;
-        tp->tags = *(uint16_t*)value;
-    } else {
-        if (is_indirect(tp)) {
-            if (tp->type == TYPE_CUSTOM) {
-                SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
-                void* ptr = (void*)sp->address;  // Usa o endereço dos dados
-                if (ptr) {
-                    memcpy(ptr, value, size);    // Copia o valor para o endereço dos dados
-                }
-            } else {
-                FREE(get_real_pointer(tp));
-                void* ptr = CALLOC(size);
-                if (ptr) {
-                    memcpy(ptr, value, size);
-                    tp->address = (uintptr_t)ptr & ((1ULL << ADDRESS_BITS) - 1);
-                }
-            }
-        } else if (type == TYPE_CUSTOM && is_indirect(tp)) {
-            SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
-            void* ptr = CALLOC(size);
-            if (ptr) {
-                memcpy(ptr, value, size);
-                sp->address = (uintptr_t)ptr;
-            }
-        }
-    }
-    tp->type = type;
 }
 
 static inline void ref(TaggedPointer* tp) {
-    if (is_indirect(tp) && tp->type == TYPE_CUSTOM) {
+    if (tp->is_compound && is_indirect(tp) && tp->tags == 1) {
         SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
         sp->ref_count++;
     }
 }
 
-/* ((sp->ref_count--) == 1)vai gerar:
-        movq    -16(%rbp), %rax
-        lock    decq    (%rax)
-        sete    %al
-        testb   $1, %al
-        je      .LBB5_6
-*/
-/*  (atomic_fetch_sub(&sp->ref_count, 1) == 1) vai gerar:
-        movq    $1, -24(%rbp)
-        movq    -24(%rbp), %rax
-        negq    %rax
-        lock    xaddq   %rax, (%rcx)
-        movq    %rax, -32(%rbp)
-        cmpq    $1, -32(%rbp)
-        jne     .LBB5_6
-*/
-// Devido ao fato do clando colocar o lock no lugar correto com decq e ainda ler corretamente o valor do ref_count anterior optei por usar a primeira opção.
 static inline void deref(TaggedPointer* tp) {
-    if (is_indirect(tp) && tp->type == TYPE_CUSTOM) {
+    if (tp->is_compound && is_indirect(tp) && tp->tags == 1) {
         SharedPointer* sp = (SharedPointer*)get_real_pointer(tp);
-        if ((sp->ref_count--) == 1) {
+        if (--sp->ref_count == 0) {
             FREE((void*)sp->address);
             FREE(sp);
-            tp->address = 0; // Reseta o ponteiro após desalocação
-            tp->type = TYPE_NULL;
+            tp->address = 0;
         }
     }
 }
 
-// Desalocação
 static inline void dealloc(TaggedPointer* tp) {
-    if (is_null(tp)) return;
-    if (is_error(tp) && is_indirect(tp)) {
-        Error* err = (Error*)tp->address;
-        FREE(err->message);
-        FREE(err);
-    } else if (is_indirect(tp)) {
-        FREE(get_real_pointer(tp));
+    if (is_indirect(tp)) {
+        if (tp->is_error) {
+            Error* err = (Error*)get_real_pointer(tp);
+            FREE(err->message);
+            FREE(err);
+        } else if (tp->tags == 1) {  // SharedPointer
+            deref(tp);
+        } else {
+            FREE(get_real_pointer(tp));
+        }
     }
     tp->raw = 0;
 }
 
-// Exemplo de Uso
 int main() {
     #if defined(__x86_64__) || defined(__aarch64__)
     #if CHECK_CPU
@@ -302,63 +288,108 @@ int main() {
     #endif
     #endif
 
-    printf("Exemplo 1: Valor nas Tags \n");
-    TaggedPointer tp1 = create(0, 42, TYPE_INT);
+    // Exemplo 1: Valor Escalar (int)
+    printf("\nExemplo 1: Valor Escalar (int)\n");
+    TaggedPointer tp1 = create(false, 0, 0);
     printf("create: "); INFO(&tp1);
-    set_value(&tp1, &(int){100}, sizeof(int), TYPE_INT);
+    int val1 = 100;
+    set_value(&tp1, &val1, sizeof(int));
     printf("setValue: "); INFO(&tp1);
     dealloc(&tp1);
     printf("dealloc: "); INFO(&tp1);
 
-    printf("Exemplo 2: Valor no Heap\n");
-    float f = 3.14;
-    TaggedPointer tp2 = create(sizeof(float), 2, TYPE_FLOAT);
+    // Exemplo 2: Valor Escalar (float)
+    printf("\nExemplo 2: Valor Escalar (float)\n");
+    TaggedPointer tp2 = create(false, 0, 0);
     printf("create: "); INFO(&tp2);
-    set_value(&tp2, &f, sizeof(float), TYPE_FLOAT);
+    float val2 = 3.14f;
+    set_value(&tp2, &val2, sizeof(float));
     printf("setValue: "); INFO(&tp2);
     dealloc(&tp2);
     printf("dealloc: "); INFO(&tp2);
 
-    printf("Exemplo 3: Erro\n");
-    TaggedPointer tp3 = create(0, 0, TYPE_NULL);
+    // Exemplo 3: Erro
+    printf("\nExemplo 3: Erro\n");
+    TaggedPointer tp3 = create(false, 0, 0);
     printf("create: "); INFO(&tp3);
     set_error(&tp3, 404, "Not Found");
     printf("set_error: "); INFO(&tp3);
     dealloc(&tp3);
     printf("dealloc: "); INFO(&tp3);
 
-    printf("Exemplo 4: Array\n");
-    int arr[] = {1, 2, 3};
-    TaggedPointer tp4 = create(sizeof(int) * 3, 3, TYPE_ARRAY);
+    // Exemplo 4: Composto (array)
+    printf("\nExemplo 4: Composto (array)\n");
+    TaggedPointer tp4 = create(1, sizeof(int) * 3, 3);
     printf("create: "); INFO(&tp4);
-    set_value(&tp4, arr, sizeof(int) * 3, TYPE_ARRAY);
+    int arr[] = {1, 2, 3};
+    set_value(&tp4, arr, sizeof(int) * 3);
     printf("setValue: "); INFO(&tp4);
-    printf("Array: [%d, %d, %d]\n", ((int*)get_real_pointer(&tp4))[0], ((int*)get_real_pointer(&tp4))[1], ((int*)get_real_pointer(&tp4))[2]);
+    void* ptr4 = get_real_pointer(&tp4);
+    if (ptr4) {
+        int* arr_ptr = (int*)ptr4;
+        printf("Array: [%d, %d, %d]\n", arr_ptr[0], arr_ptr[1], arr_ptr[2]);
+    }
     dealloc(&tp4);
     printf("dealloc: "); INFO(&tp4);
 
-    printf("Exemplo 5: SharedPointer com ref_count\n");
-    TaggedPointer tp5 = create(sizeof(int), 0, TYPE_CUSTOM);
+    // Exemplo 5: Composto (array de floats)
+    printf("\nExemplo 5: Composto (array de floats)\n");
+    TaggedPointer tp5 = create(true, sizeof(float) * 2, 2);
     printf("create: "); INFO(&tp5);
-    int value = 42;
-    set_value(&tp5, &value, sizeof(int), TYPE_CUSTOM);
+    float arr5[] = {1.1f, 2.2f};
+    set_value(&tp5, arr5, sizeof(float) * 2);
     printf("setValue: "); INFO(&tp5);
-    ref(&tp5);
-    printf("ref: "); INFO(&tp5);
-    deref(&tp5);
-    printf("deref: "); INFO(&tp5);
-    deref(&tp5);
-    printf("deref: "); INFO(&tp5);
+    void* ptr5 = get_real_pointer(&tp5);
+    if (ptr5) {
+        float* arr_ptr5 = (float*)ptr5;
+        printf("Array: [%f, %f]\n", arr_ptr5[0], arr_ptr5[1]);
+    }
+    dealloc(&tp5);
+    printf("dealloc: "); INFO(&tp5);
 
-    printf("Exemplo 6: String\n");
-    char str[] = "Hello World!";
-    TaggedPointer tp6 = create(sizeof(str), 0, TYPE_CHAR);
+    // Exemplo 6: Composto (string)
+    printf("\nExemplo 6: Composto (string)\n");
+    TaggedPointer tp6 = create(true, strlen("Hello World!") + 1, strlen("Hello World!"));
     printf("create: "); INFO(&tp6);
-    set_value(&tp6, str, sizeof(str), TYPE_CHAR);
+    char str[] = "Hello World!";
+    set_value(&tp6, str, sizeof(str));
     printf("setValue: "); INFO(&tp6);
-    printf("String: %s\n", (char*)get_real_pointer(&tp6));
+    void* ptr6 = get_real_pointer(&tp6);
+    if (ptr6) {
+        printf("String: %s\n", (char*)ptr6);
+    }
     dealloc(&tp6);
     printf("dealloc: "); INFO(&tp6);
+
+    // Exemplo 7: Valor Escalar (uint64_t)
+    printf("\nExemplo 7: Valor Escalar (uint64_t)\n");
+    TaggedPointer tp7 = create(false, 0, 0);
+    printf("create: "); INFO(&tp7);
+    uint64_t val7 = 1234567890123456789ULL;
+    set_value(&tp7, &val7, sizeof(uint64_t));
+    printf("setValue: "); INFO(&tp7);
+    dealloc(&tp7);
+    printf("dealloc: "); INFO(&tp7);
+
+    // Exemplo 8: Composto com SharedPointer
+    printf("\nExemplo 8: Composto com SharedPointer\n");
+    SharedPointer* sp = CALLOC(sizeof(SharedPointer));
+    sp->ref_count = 1;
+    sp->type = 0;
+    sp->address = (uintptr_t)CALLOC(sizeof(int));
+    *(int*)sp->address = 42;
+    TaggedPointer tp8 = { .raw = 0 };
+    tp8.is_compound = 1;
+    tp8.is_error = 0;
+    tp8.address = (uintptr_t)sp & ((1ULL << ADDRESS_BITS) - 1);
+    tp8.tags = 1;
+    printf("create: "); INFO(&tp8);
+    ref(&tp8);
+    printf("ref: "); INFO(&tp8);
+    deref(&tp8);
+    printf("deref: "); INFO(&tp8);
+    deref(&tp8);
+    printf("deref: "); INFO(&tp8);
 
     return 0;
 }
